6e6a1f6ec7a4de4e93a2608c0193833bfcb22389
SebasElElotero <sebas11121314151617181920@hotmail.com>
2024-11-29 14:56:16 -0600

    Actulizar recomendaciones

--- recomendaciones/utils.py
+++ recomendaciones/utils.py
@@ -5,13 +5,20 @@ from .models import Recomendacion
 def obtener_recomendaciones_para_usuario(request):
     # Obtener los productos que el usuario ya compró
     #print(request.__dict__)
-    pedido = Recomendacion.objects.filter(usuario=request).last()
+    recomendaciones = Recomendacion.objects.filter(usuario=request)
+    print(recomendaciones)
     #itemspedido = ItemPedido.objects.filter(pedido=pedido)
     #print(itemspedido)
-    print(pedido.__dict__)
-    productos_comprados = Producto.objects.filter(id=pedido.producto_id).distinct()
+    #print(pedido.__dict__)
+    productos = []
+    for recomendacion in recomendaciones:
+        productos_comprados = Producto.objects.filter(id=recomendacion.producto_id).distinct()
+        productos.append(productos_comprados)
+    print(productos)
+    #print(productos_comprados)
 
     # Excluir los productos ya comprados de las recomendaciones
-    productos_recomendados = Producto.objects.exclude(id__in=productos_comprados).order_by('?')[:5]  # Recomienda 5 productos al azar
-    print(productos_recomendados)
-    return productos_recomendados
+    #productos_recomendados = Producto.objects.exclude(id__in=productos_comprados)  # Recomienda 5 productos al azar
+    #print(productos_recomendados)
+    #return productos_recomendados
+    return productos
--- recomendaciones/views.py
+++ recomendaciones/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from .utils import obtener_recomendaciones_para_usuario
 
 def ver_recomendaciones(request):
+    print(request.user)
     recomendaciones = obtener_recomendaciones_para_usuario(request.user)
-    print(recomendaciones)  # Verifica qué devuelve esta función
-    return render(request, 'recomendaciones/ver_recomendaciones.html', {'recomendaciones': recomendaciones})
+    productos = []
+    for producto in recomendaciones:
+        productos.append(producto[0])
+    print("Recomendaciones")
+    print(productos)  # Verifica qué devuelve esta función
+    return render(request, 'recomendaciones/ver_recomendaciones.html', {'recomendaciones': productos})
